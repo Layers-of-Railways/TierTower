3465b95adbc5c7c2208a2e0feee593e5a59e2d68
techno-sam <77073745+techno-sam@users.noreply.github.com>
2025-12-27 01:54:48 -0800

    update release script
    [ci skip]

--- release.sh
+++ release.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Creates a tagged version. Releases must be manually created.
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 \"<release message>\""
+  exit 1
+fi
+
 version=$(grep -E 'mod_version(\s*)=(\s*)' "./gradle.properties" | cut -d'=' -f2 | tr -d ' ')
+
+echo "Creating tag for version $version with message '$1'"
 
 git tag -s -a v"$version" -m "$1"
 
-git push origin main v"$version"
+git push origin 1.20.1/dev v"$version"
